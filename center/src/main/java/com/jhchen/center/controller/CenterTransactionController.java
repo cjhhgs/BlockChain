5239fe824067a41461ef910f31e66558b3b90a5a
jhchen <462072107@qq.com>
2023-04-20 19:15:26 +0800

    1

--- center/src/main/java/com/jhchen/center/controller/CenterTransactionController.java
+++ center/src/main/java/com/jhchen/center/controller/CenterTransactionController.java
@@ -5,6 +5,7 @@ import com.jhchen.framework.domain.ResponseResult;
 import com.jhchen.framework.domain.modul.SignedTransaction;
 import com.jhchen.framework.domain.modul.TransactionPool;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,13 +25,21 @@ public class CenterTransactionController {
     TransactionPool transactionPool;
 
     @PostMapping("/addTransaction")
+    @ApiOperation(value = "接收一个新的交易")
     public ResponseResult addTransaction(@RequestBody SignedTransaction transaction){
         return centerService.addTransaction(transaction);
     }
 
     @GetMapping("/showTransactions")
+    @ApiOperation(value = "显示交易列表")
     public ResponseResult showTransactions(){
         return ResponseResult.okResult(transactionPool);
     }
 
+    @GetMapping("/allocTransaction")
+    @ApiOperation(value = "自动分配任务")
+    public ResponseResult allocTransaction(){
+        return null;
+        //TODO:
+    }
 }
--- framework/src/main/java/com/jhchen/framework/domain/modul/TransactionPool.java
+++ framework/src/main/java/com/jhchen/framework/domain/modul/TransactionPool.java
@@ -20,11 +20,20 @@ public class TransactionPool {
     //已完成交易池
     private List<TransactionPoolItem> finishedTransactionList;
 
+    /**
+     * 接收一笔新的交易
+     * @param transaction
+     */
     public void add(SignedTransaction transaction){
         transactionList.add(transaction);
     }
 
 
+    /**
+     * 获取n笔未分配的交易
+     * @param num
+     * @return
+     */
     public List<SignedTransaction> getUnallocated(Integer num){
         List<SignedTransaction> res = new ArrayList<>();
         if(num> transactionList.size()){
@@ -34,9 +43,35 @@ public class TransactionPool {
             res.add(transactionList.poll());
         }
         return res;
-
     }
 
+
+    /**
+     * 分配给一个人
+     * @param transaction
+     * @param minerAddr
+     */
+    public void allocate(SignedTransaction transaction, String minerAddr){
+        //在未分配中寻找是否有
+        Iterator<SignedTransaction> iterator = transactionList.iterator();
+        Boolean flag = false;
+        while (iterator.hasNext()){
+            SignedTransaction next = iterator.next();
+            if(next.equals(transaction))
+                iterator.remove();
+                break;
+        }
+        //加入已分配
+        allocatedTransactionList.add(new TransactionPoolItem(transaction,minerAddr));
+    }
+
+
+    /**
+     * 在已分配队列中寻找
+     * @param transaction
+     * @param minerAddr
+     * @return
+     */
     public boolean find(SignedTransaction transaction,String minerAddr){
         Iterator<TransactionPoolItem> iterator = allocatedTransactionList.iterator();
         while (iterator.hasNext()){
